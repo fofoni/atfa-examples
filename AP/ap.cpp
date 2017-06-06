119bdd8a4e2e3c8d57d1863297ffba3f9d6319b7
Pedro Angelo <pedro.fonini@smt.ufrj.br>
2017-06-06 07:03:48 -0300

    AP: begin

--- AP/ap.cpp
+++ AP/ap.cpp
@@ -19,31 +19,45 @@
 using sample_t = float;
 
 constexpr int N = 64;
+constexpr int M = 2;
 constexpr sample_t mu = 0.9;
 constexpr sample_t delta = std::sqrt(std::numeric_limits<sample_t>::epsilon());
 
 struct AdapfData {
 
-    sample_t x_array[N];
-    Eigen::Map<Eigen::Matrix<sample_t, N, 1>> x;
-
+    sample_t x_array[N*M];
     sample_t w_array[N];
+    sample_t e_array[M];
+
+    Eigen::Map<Eigen::Matrix<sample_t, N, M>> X; // input matrix
+    Eigen::Map<Eigen::Matrix<sample_t, N, 1>> x; // first column of X
     Eigen::Map<Eigen::Matrix<sample_t, N, 1>> w;
+    Eigen::Map<Eigen::Matrix<sample_t, 1, M>> err;
 
     AdapfData()
-        : x(x_array), w(w_array)
+        : X(x_array), x(x_array), w(w_array), err(e_array)
     {
         reset();
     }
 
     void reset() {
-        std::fill(x_array, x_array+N, 0);
+        std::fill(x_array, x_array+N*M, 0);
         std::fill(w_array, w_array+N, 0);
+        std::fill(e_array, e_array+M, 0);
     }
 
     void push(sample_t sample) {
-        std::copy_backward(x_array, x_array+(N-1), x_array+N); // shift down
-        x_array[0] = sample; // push at top
+        // shift columns to the right
+        std::copy_backward(x_array, x_array+(M-1)*N, x_array+N*M);
+        // shift first column downwards
+        std::copy_backward(x_array, x_array+(N-1), x_array+N)
+        // push at top-left corner
+        x_array[0] = sample;
+    }
+
+    void push_err(sample_t sample) {
+        std::copy_backward(e_array, e_array+(M-1), e_array+M);
+        e_array[0] = sample;
     }
 
     sample_t dot_product() const {
@@ -82,7 +96,8 @@ float adapf_run(AdapfData *data, float sample, float y, int update,
                 int *updated)
 {
     data->push(sample);
-    sample_t err = y - data->dot_product();
+    data->push_err(y - data->dot_product());
+    // TODO
     if (update)
         data->update(err);
     *updated = update;
